545b65c91c48bc93bb28770a1802a4d280b4cb72
guilherme-alc <guilhermealc01@gmail.com>
2025-07-25 22:23:00 -0300

    Adiciona classe user

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace JwtAspnet.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string GivenName { get; set; }
+        public string Image { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
