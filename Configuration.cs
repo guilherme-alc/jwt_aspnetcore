0ac386aba1cf3a2f6430f800f32ffbd865f49c69
guilherme-alc <guilhermealc01@gmail.com>
2025-07-25 22:22:37 -0300

    Adiciona chave privada do token

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtAspnet
+{
+    public static class Configuration
+    {
+        public static string PrivateKey { get; private set; } = "SDMSA389@)389G,CS8[]23;F923~]DSA[Q927SDK2S";
+    }
+}
