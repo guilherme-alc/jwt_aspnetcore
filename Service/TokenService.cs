828aa004e14ca05e81b0461dd7079f325fc405ec
guilherme-alc <guilhermealc01@gmail.com>
2025-07-25 22:24:24 -0300

    Implementa método que gera token

---
+++ Service/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JwtAspnet.Service
+{
+    public class TokenService
+    {
+        public string Generate()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.PrivateKey));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = credentials,
+                Expires = DateTime.UtcNow.AddHours(6),
+            };
+
+            var token = handler.CreateToken(tokenDescriptor);
+
+            return handler.WriteToken(token);
+        }
+    }
+}
