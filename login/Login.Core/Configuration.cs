6c725ee4d59e926d922555eac9bf0e5293467f60
guilherme-alc <guilhermealc01@gmail.com>
2025-07-27 10:30:41 -0300

    Adiciona VO para Password com: - Método para geração de senhas fortes; - Hashing para armazenar as senhas; - Método para verificar senha e validar no login;

---
+++ login/Login.Core/Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace Login.Core
+{
+    public static class Configuration
+    {
+        public static SecretsConfiguration Secrets { get; set; } = new();
+        public class SecretsConfiguration
+        {
+            public string PasswordSaltKey { get; set; } = string.Empty;
+        }
+    }
+}
