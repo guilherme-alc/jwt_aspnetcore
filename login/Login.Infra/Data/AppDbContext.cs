f6edbe74c43327eaa5732e0564c6565887a8b77a
guilherme-alc <guilhermealc01@gmail.com>
2025-07-27 19:28:59 -0300

    Cria DataContext

---
+++ login/Login.Infra/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Login.Core.Contexts.AccountContext.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Infra.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
