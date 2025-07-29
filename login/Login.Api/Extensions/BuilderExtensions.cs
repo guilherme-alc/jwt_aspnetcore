fd75bda82cff5b1df91a750f88ddf67ff89e7003
guilherme-alc <guilhermealc01@gmail.com>
2025-07-28 21:25:41 -0300

    Configura Api, criando método de extensão do builder para organizar o program.cs

---
+++ login/Login.Api/Extensions/BuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Login.Core;
+using Login.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Api.Extensions
+{
+    public static class BuilderExtensions
+    {
+        public static void AddConfiguration(this WebApplicationBuilder builder)
+        {
+            Configuration.Database.ConnectionString = builder.Configuration
+                .GetConnectionString("DefaultConnection") ?? string.Empty;
+
+            Configuration.Secrets.PasswordSaltKey = builder.Configuration
+                .GetSection("Secrets")
+                .GetValue<string>("PasswordSaltKey") ?? string.Empty;
+        }
+
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.Database.ConnectionString, b =>
+                {
+                    b.MigrationsAssembly("Login.Api");
+                });
+            });
+        }
+    }
+}
--- login/Login.Core/Configuration.cs
+++ login/Login.Core/Configuration.cs
@@ -3,9 +3,15 @@
     public static class Configuration
     {
         public static SecretsConfiguration Secrets { get; set; } = new();
+        public static DatabaseConfiguration Database { get; set; } = new();
         public class SecretsConfiguration
         {
             public string PasswordSaltKey { get; set; } = string.Empty;
+        }
+
+        public class DatabaseConfiguration
+        {
+            public string ConnectionString { get; set; } = string.Empty;
         }
     }
 }
