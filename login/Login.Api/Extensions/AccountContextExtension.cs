ed43db3e6325a371ba677899e08e68fd2393aef7
guilherme-alc <guilhermealc01@gmail.com>
2025-07-29 21:18:20 -0300

    Organiza Api

---
+++ login/Login.Api/Extensions/AccountContextExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Login.Api.Extensions
+{
+    public static class AccountContextExtension
+    {
+        public static void AddAccountContext(this WebApplicationBuilder services)
+        {
+
+        }
+
+        public static void MapAccountContext(this WebApplication services)
+        {
+
+        }
+    }
+}
