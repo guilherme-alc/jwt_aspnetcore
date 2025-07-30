7d2af1d391b55455dc555130de6727983fd29709
guilherme-alc <guilhermealc01@gmail.com>
2025-07-29 21:15:03 -0300

    Implementa sendgrid no service

--- login/Login.Core/Configuration.cs
+++ login/Login.Core/Configuration.cs
@@ -4,6 +4,8 @@
     {
         public static SecretsConfiguration Secrets { get; set; } = new();
         public static DatabaseConfiguration Database { get; set; } = new();
+        public static EmailConfiguration Email { get; set; } = new();
+        public static SendGridConfiguration SendGrid { get; set; } = new();
         public class SecretsConfiguration
         {
             public string PasswordSaltKey { get; set; } = string.Empty;
@@ -12,6 +14,17 @@
         public class DatabaseConfiguration
         {
             public string ConnectionString { get; set; } = string.Empty;
+        }
+
+        public class EmailConfiguration
+        {
+            public string DefaultFromEmail { get; set; } = "guilhermealc01@gmail.com";
+            public string DefaultFromName { get; set; } = "Guilherme Campos";
+        }
+
+        public class SendGridConfiguration
+        {
+            public string ApiKey { get; set; } = string.Empty;
         }
     }
 }
---
+++ login/Login.Infra/Contexts/AccounContext/UseCases/CreateAccountService.cs
@@ -0,0 +1,22 @@
+﻿using Login.Core;
+using Login.Core.Contexts.AccountContext.Entities;
+using Login.Core.Contexts.AccountContext.UseCases.CreateAccount.Contracts;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Login.Infra.Contexts.AccounContext.UseCases
+{
+    public class CreateAccountService : ICreateAccountService
+    {
+        public async Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
+        {
+            var client = new SendGridClient(Configuration.SendGrid.ApiKey);
+            var from = new EmailAddress(Configuration.Email.DefaultFromEmail, Configuration.Email.DefaultFromName);
+            const string subject = "Verifique sua conta";
+            var to = new EmailAddress(user.Email, user.Name);
+            var content = $"Código {user.Email.Verification.Code}";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+            await client.SendEmailAsync(msg, cancellationToken);
+        }
+    }
+}
